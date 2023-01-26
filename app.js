b077f5d5f99f5a6ee6eb15f3391a30d574d9d7a6
Juljeta Troka <juliet.troka@gmail.com>
2023-01-26 18:37:45 +0000

    inserted functionality to the new card button

--- app.js
+++ app.js
@@ -6,32 +6,43 @@ let text = document.querySelector("#text");
 let startBtn = document.querySelector("#start-btn");
 let newCardBtn = document.querySelector("#card-btn");
 
-
 let firstNum = randomNum();
 let secondNum = randomNum();
 let sum = firstNum + secondNum;
 let message = "";
 let isAlive = false;
+let gameOver = false;
 
 function startGame(){
     if (sum < 21) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
+        message = "You've got Blackjack!";
         isAlive= true;
     } else if (sum > 21) {
-        message = "You are out of the game!"
+        message = "You are out of the game!";
+        gameOver = true;
     }
 
 
-    text.textContent = message
-    cardsText.textContent = `Cards: ${firstNum} ${secondNum}`
-    sumText.textContent = `Sum: ${sum}`
+    text.textContent = message;
+    cardsText.textContent = `Cards: ${firstNum} ${secondNum}`;
+    sumText.textContent = `Sum: ${sum}`;
 }
 
 function randomNum(){
-    return Math.floor(Math.random() * 12) + 1
+    return Math.floor(Math.random() * 12) + 1;
 }
 
-startBtn.addEventListener("click", startGame)
+function newCard(){
+    let card = randomNum()
+    sum += card
+
+    cardsText.textContent += " "+ card;
+    sumText.textContent = `Sum: ${sum}`;
+    console.log(sum)
+}
+
+startBtn.addEventListener("click", startGame);
+newCardBtn.addEventListener("click", newCard);
 console.log(isAlive)
