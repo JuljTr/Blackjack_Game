fee8bbce85a981eab54700991f52e6bc322deb20
Juljeta Troka <juliet.troka@gmail.com>
2023-01-26 18:27:41 +0000

    generated the random numbers function

--- app.js
+++ app.js
@@ -7,25 +7,31 @@ let startBtn = document.querySelector("#start-btn");
 let newCardBtn = document.querySelector("#card-btn");
 
 
-let firstNum = 10;
-let secondNum = 11;
+let firstNum = randomNum();
+let secondNum = randomNum();
 let sum = firstNum + secondNum;
 let message = "";
+let isAlive = false;
 
 function startGame(){
     if (sum < 21) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
+        isAlive= true;
     } else if (sum > 21) {
         message = "You are out of the game!"
     }
+
+
     text.textContent = message
-    
     cardsText.textContent = `Cards: ${firstNum} ${secondNum}`
-    
     sumText.textContent = `Sum: ${sum}`
 }
 
+function randomNum(){
+    return Math.floor(Math.random() * 12) + 1
+}
+
 startBtn.addEventListener("click", startGame)
-console.log(sum)
+console.log(isAlive)
