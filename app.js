74a9ce4f7d1f9117523775865b3a6520f40b0ca6
Juljeta Troka <juliet.troka@gmail.com>
2023-01-26 17:50:56 +0000

    initial commmit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+let cardsText = document.querySelector("#cards-text");
+let sumText = document.querySelector("#cards-sum");
+let text = document.querySelector("#text");
+
+
+let firstNum = 10;
+let secondNum = 11;
+let sum = firstNum + secondNum;
+
+console.log(sum)
