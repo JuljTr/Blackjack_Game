5d5bb268fe3bd8af9785548f818fb161c38ad955
Juljeta Troka <juliet.troka@gmail.com>
2023-01-26 18:17:43 +0000

    initiated the logic of the game and added the buttons

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ console.log("hello world")
 let cardsText = document.querySelector("#cards-text");
 let sumText = document.querySelector("#cards-sum");
 let text = document.querySelector("#text");
+let startBtn = document.querySelector("#start-btn");
+let newCardBtn = document.querySelector("#card-btn");
 
 
 let firstNum = 10;
 let secondNum = 11;
 let sum = firstNum + secondNum;
+let message = "";
 
+function startGame(){
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+    } else if (sum > 21) {
+        message = "You are out of the game!"
+    }
+    text.textContent = message
+    
+    cardsText.textContent = `Cards: ${firstNum} ${secondNum}`
+    
+    sumText.textContent = `Sum: ${sum}`
+}
+
+startBtn.addEventListener("click", startGame)
 console.log(sum)
